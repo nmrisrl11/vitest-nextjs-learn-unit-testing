4164de184186a5769dff780b4bf2bc1b78b7708c
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-03-28 22:16:13 +0800

    chore: install and setup jest-dom

---
+++ app/page.test.tsx
@@ -0,0 +1,14 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Home from "./page";
+
+describe("Home Page", () => {
+	it("should renders an H1 with correct title", () => {
+		render(<Home />);
+
+		const heading = screen.getByRole("heading", { level: 1 });
+
+		expect(heading).toBeInTheDocument();
+		expect(heading).toHaveTextContent(/welcome/i);
+	});
+});
