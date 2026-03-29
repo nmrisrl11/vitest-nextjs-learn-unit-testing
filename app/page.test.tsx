5473c688f2ac735ff50f52d7370293bac4f8b646
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-03-29 09:50:02 +0800

    test: add test cases to Home page

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Home from "./page";
 
@@ -10,5 +11,29 @@ describe("Home Page", () => {
 
 		expect(heading).toBeInTheDocument();
 		expect(heading).toHaveTextContent(/welcome/i);
+	});
+
+	it("should initially does not show the More Features list", () => {
+		render(<Home />);
+
+		const toggleMoreButton = screen.getByRole("button", { name: /show more/i });
+		expect(toggleMoreButton).toBeInTheDocument();
+
+		const moreFeatureList = screen.queryByRole("list", { name: /more features/i });
+		expect(moreFeatureList).not.toBeInTheDocument();
+	});
+
+	it("should shows the More Features list when the button is clicked", async () => {
+		render(<Home />);
+
+		const toggleMoreButton = screen.getByRole("button", { name: /show more/i });
+
+		const user = userEvent.setup();
+		await user.click(toggleMoreButton);
+
+		const moreFeatureList = screen.getByRole("list", { name: /more features/i });
+		expect(moreFeatureList).toBeInTheDocument();
+
+		expect(toggleMoreButton).toHaveTextContent(/show less/i);
 	});
 });
