09e332c2838e837d48dbc5f8f875ebb6b61ba92b
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-03-29 09:31:32 +0800

    test: add setup function to ThemedButton component

--- components/ThemedButton/ThemedButton.test.tsx
+++ components/ThemedButton/ThemedButton.test.tsx
@@ -1,9 +1,17 @@
 import { render, screen } from "@testing-library/react";
-import useEvent from "@testing-library/user-event";
+import { userEvent } from "@testing-library/user-event";
 
-import ThemedButton from "./ThemedButton";
+import ThemedButton, { ThemedButtonProps } from "./ThemedButton";
 
 describe("ThemedButton component", () => {
+	const setupThemedButton = (overrides: Partial<ThemedButtonProps> = {}) => {
+		return {
+			user: userEvent.setup(),
+			...render(<ThemedButton {...overrides}>Click</ThemedButton>),
+			button: screen.getByRole("button"),
+		};
+	};
+
 	it.each(["primary", "secondary", "danger"])(
 		"should render ThemedButton with %s variant color",
 		(variant) => {
@@ -18,10 +26,8 @@ describe("ThemedButton component", () => {
 	it("should call onClick handler when clicked", async () => {
 		const handleClick = vi.fn();
 
-		render(<ThemedButton onClick={handleClick}>Click</ThemedButton>);
+		const { user, button } = setupThemedButton({ onClick: handleClick });
 
-		const button = screen.getByRole("button");
-		const user = useEvent.setup();
 		await user.click(button);
 
 		expect(handleClick).toHaveBeenCalledTimes(1);
@@ -30,14 +36,8 @@ describe("ThemedButton component", () => {
 	it("should not call onClick handler when disabled", async () => {
 		const handleClick = vi.fn();
 
-		render(
-			<ThemedButton onClick={handleClick} disabled>
-				Click
-			</ThemedButton>,
-		);
+		const { user, button } = setupThemedButton({ onClick: handleClick, disabled: true });
 
-		const button = screen.getByRole("button");
-		const user = useEvent.setup();
 		await user.click(button);
 
 		expect(handleClick).not.toHaveBeenCalled();
