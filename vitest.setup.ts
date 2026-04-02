d262749b4c475f013227c99d8ff55313b09c20c5
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-04-02 11:55:54 +0800

    test: mock next navigation

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,5 +5,20 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 import { server } from "./__mocks__/node";
 
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+	server.resetHandlers();
+	vi.resetAllMocks();
+});
 afterAll(() => server.close());
+
+vi.mock("next/navigation", () => {
+	const push = vi.fn();
+	const replace = vi.fn();
+	const back = vi.fn();
+	const forward = vi.fn();
+	const refresh = vi.fn();
+
+	return {
+		useRouter: () => ({ push, replace, back, forward, refresh }),
+	};
+});
